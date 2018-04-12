5b1c5c266aef90ef6f5f753051aaf8ba24f9f8ee
magical2world <13390100342@163.com>
2018-04-12 15:10:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import config
 from model import network
 #from InceptionV3 import network
-def main(args,train=False):
-    if train:
+def main(args):
+    if args.train:
         image=tf.placeholder(tf.float32,[args.batch_size,224,224,3])
         sentence=tf.placeholder(tf.int32,[args.batch_size,None])
     else:
@@ -11,7 +11,7 @@ def main(args,train=False):
         image=tf.placeholder(tf.float32,[1,224,224,3])
         sentence=tf.placeholder(tf.int32,[1,None])
     model=network(image,sentence,args)
-    if train:
+    if args.train:
         model.train_network()
     else:
         model.test_network()
