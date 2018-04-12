6f19ba0027ebb4885e3f045f816fa4f4459c2b07
magical2world <13390100342@163.com>
2018-04-12 15:17:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import argparse
 
 def config():
     parser=argparse.ArgumentParser()
-    parser.add_argument("--model",type=str,default='train',help="train or test")
+    parser.add_argument("--mode",type=str,default='train',help="train or test")
     parser.add_argument("--batch_size",type=int,default=64,help="batch size for training")
     parser.add_argument("--learning_rate",type=float,default=0.0001,help="learning rate")
     parser.add_argument("--train_step",type=int,default=40000,help="number of step to train")
