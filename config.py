004e64dac88aa64253f6e03382f484c3d327b035
magical2world <13390100342@163.com>
2018-04-12 15:02:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import argparse
+
+def config():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--model",type=str,default='train',help="train or test")
+    parser.add_argument("--batch_size",type=int,default=64,help="batch size for training")
+    parser.add_argument("--learning_rate",type=float,default=0.0001,help="learning rate")
+    parser.add_argument("--train_step",type=int,default=40000,help="number of step to train")
+    parser.add_argument("--hidden_layers",type=int,default=3,help="rnn hidden layers")
+    parser.add_argument("--hidden_units",type=int,default=512,help="rnn hidden units")
+
+    return parser.parse_args()
